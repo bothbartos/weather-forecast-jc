2b417fcee3526f9f84c17298f6e4f00a742f5d12
bartosboth <bothbartos@gmail.com>
2025-02-25 13:39:31 +0100

    added better styling for hourly lazy row

--- app/src/main/java/com/bartosboth/weatherforecast/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/bartosboth/weatherforecast/ui/screens/detail/DetailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -105,23 +107,44 @@ fun DetailContent(modifier: Modifier = Modifier, forecastDay: Forecastday, isImp
 
 @Composable
 fun HourlyCard(hour: Hour, isImperial: Boolean) {
-    Card(modifier = Modifier.padding(5.dp),
-        shape = RoundedCornerShape(3.dp)) {
-        Column(modifier = Modifier.fillMaxWidth().padding(3.dp)) {
+    Card(shape = CircleShape,
+        modifier = Modifier.padding(3.dp),
+        colors = CardColors(containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+            disabledContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            )){
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween) {
             Text(
                 text = if (isImperial) {
                     hour.temp_f.roundToInt().toString() + "°F"
                 } else {
                     hour.temp_c.roundToInt().toString() + "°C"
-                }
+                },
+                style = TextStyle(
+                    fontStyle = MaterialTheme.typography.bodySmall.fontStyle,
+                    fontWeight = MaterialTheme.typography.bodySmall.fontWeight
+                )
             )
             WeatherStateImage(
                 imageUrl = "https:${hour.condition.icon}",
                 imageSize = 50
             )
-            Text(text = hour.time.split(" ")[1])
+            Text(
+                text = hour.time.split(" ")[1],
+                style = TextStyle(
+                    fontStyle = MaterialTheme.typography.bodySmall.fontStyle,
+                    fontWeight = MaterialTheme.typography.bodySmall.fontWeight
+                )
+            )
+
         }
     }
+
 }
 
 @Composable
@@ -130,7 +153,13 @@ fun HourlyLazyRow(data: List<Hour>, isImperial: Boolean) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(14.dp),
-        shape = RoundedCornerShape(27.dp)
+        shape = RoundedCornerShape(27.dp),
+        colors = CardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+            disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+        )
     ) {
         Text(
             text = "Hourly forecast",
@@ -138,9 +167,9 @@ fun HourlyLazyRow(data: List<Hour>, isImperial: Boolean) {
             modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth(),
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Left
         )
-        LazyRow() {
+        LazyRow(modifier = Modifier.padding(7.dp)) {
             items(data.size) { index ->
                 HourlyCard(data[index], isImperial = isImperial)
             }
