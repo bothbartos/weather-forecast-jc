03dd474b80f45d01a53fd448acd6fcb9d1e239fc
bartosboth <bothbartos@gmail.com>
2025-02-25 15:35:09 +0100

    added mockito dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,6 +115,9 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    //Mockito
+    androidTestImplementation(libs.mockito.core)
+    androidTestImplementation(libs.mockito.kotlin)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
